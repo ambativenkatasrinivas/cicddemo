adc3b121a8bf61f310722fe3dd8ba3f87498fe9f
ambativenkatasrinivas <ambati.srinivas45@gmail.com>
2017-07-24 15:06:27 +0530

    controller chenged

--- src/main/java/com/sringboot/EmployeeController.java
+++ src/main/java/com/sringboot/EmployeeController.java
@@ -25,7 +25,7 @@ public class EmployeeController {
     public List<Employee> getEmployees() 
     {
 		List<Employee> employeesList = new ArrayList<Employee>();
-		employeesList.add(new Employee(1,"lokesh","gupta","howtodoinjava@gmail.com"));
+		employeesList.add(new Employee(1,"lokesh","gupta","srinivas@a.com"));
 		return employeesList;
     }
 	
